514affab892fcbf8837c564c756a405d2fee47ec
Nickolay2312 <61413110+Nickolay2312@users.noreply.github.com>
2022-06-18 21:47:24 +0300

    Валидация

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -8,12 +8,18 @@
         <div class="row mb-3">
             <label for="name" class="col-sm-2 col-form-label">{{__('Name')}}</label>
             <div class="col-sm-10">
+                @error('name')
+                <div class="alert alert-danger pt-1 pb-1">{{$message}}</div>
+                @enderror
                 <input type="text" name="name" class="form-control" id="name" value="{{$category->name}}">
             </div>
         </div>
         <div class="row mb-3">
             <label for="code" class="col-sm-2 col-form-label">{{__('Code')}}</label>
             <div class="col-sm-10">
+                @error('code')
+                <div class="alert alert-danger pt-1 pb-1">{{$message}}</div>
+                @enderror
                 <input type="text" name="code" class="form-control" id="code" value="{{$category->code}}">
             </div>
         </div>
@@ -28,19 +34,6 @@
             <label class="col-sm-2 col-form-label" for="image">{{__('Image')}}</label>
             <div class="col-sm-10">
                 <input type="file" name="image" class="form-control" id="image">
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label for="name_en" class="col-sm-2 col-form-label">{{__('English name')}}</label>
-            <div class="col-sm-10">
-                <input type="text" name="name_en" class="form-control" id="name_en" value="{{$category->name_en}}">
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label for="description_en" class="col-sm-2 col-form-label">{{__('English_description')}}</label>
-            <div class="col-sm-10">
-                <textarea name="description_en" class="form-control" id="description_en">{{$category->description_en}}
-                </textarea>
             </div>
         </div>
         <button type="submit" class="btn btn-primary">{{__('Save')}}</button>
